bec5cf50176f58e9651c9bd7a1662bec78e6f208
Turko <martin.pdv@diggap.com>
2023-08-22 12:42:04 +0200

    Source code initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://www.linkedin.com/in/martin-perez-de-varela" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Martin Perez De Varela</h1>
+      <h2>Design & Development</h2>
+    </>
+  )
+}
+
+export default App
